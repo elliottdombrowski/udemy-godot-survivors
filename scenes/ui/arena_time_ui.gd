3fc1b241a1aa7e1f166bab6fdcb1bbc53d020359
elliottdombrowski <elliottroyal@me.com>
2024-04-22 22:51:50 -0500

    format timer

--- scenes/ui/arena_time_ui.gd
+++ scenes/ui/arena_time_ui.gd
@@ -7,4 +7,10 @@ extends CanvasLayer
 func _process(delta):
 	if arena_time_manager == null: return
 	var time_elapsed = arena_time_manager.get_time_elapsed()
-	label.text = str(time_elapsed)
+	label.text = format_seconds_to_string(time_elapsed)
+
+
+func format_seconds_to_string(seconds: float):
+	var minutes           = floor(seconds / 60)
+	var remaining_seconds = floor(seconds - (minutes * 60))
+	return str(minutes) + ":" + ("%02d" % remaining_seconds)
